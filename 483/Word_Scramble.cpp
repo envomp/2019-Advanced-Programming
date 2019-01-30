d2b7a9b18faf508cca3541fefcfd9099048018b1
envomp <envomp@ttu.ee>
2019-01-30 04:54:29 +0000

    Update Word_Scramble.cpp

--- 483/Word_Scramble.cpp
+++ 483/Word_Scramble.cpp
@@ -1,5 +1,5 @@
 // Original python code...
-//
+// Removed redundent cout << endl;
 // from sys import stdin
 //
 // sisend = """I love you.
@@ -17,25 +17,17 @@ using namespace std;
 
 int main() {
 	char c;
-	char l = 'a';
 	string answer;
-	string printer;
 
 	while(scanf("%c", &c) != EOF){
 		
 		if (c == ' ' || c == '\n'){
-			answer += c;
-			printer += answer;
+			cout<<answer<<c;
 			answer = "";
-			if(l == '\n' && c == ' '){
-			answer = " ";		
-			}
+			
 		} else {
 			answer = c + answer;
 		}		
 	}
-	l = c;
-	cout<<printer<<endl;
-
 	return 0;
 }
